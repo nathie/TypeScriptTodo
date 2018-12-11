7f2c20bb404463594c898089e7c02e494af27445
Gleidy Mendoza <gleidymendoza@gmail.com>
2018-12-11 15:14:47 -0800

    Chapter 2.2: Template strings

--- app.ts
+++ app.ts
@@ -1,14 +1,14 @@
 var container = document.getElementById('container');
 
-function countdown(initial, final = 0, interval = 1) {
-
-    var current = initial;
-
-    while (current > final) {
-        container.innerHTML = current;
-        current -= interval;
-    }
-
+var todo = {
+    id: 123,
+    name: "Pick up drycleaning",
+    completed: true
 }
 
-countdown(10);
+container.innerHTML = `
+<div todo='${todo.id}' class="list-group-item}">
+  <i class="${ todo.completed ? "" : "hidden" } text-success glyphicon glyphicon-ok"></i>
+  <span class="name">${todo.name}</span>
+</div>
+`;
