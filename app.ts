1ed011a249e9492698b9b2a1cc55c90e1e439e3c
Gleidy Mendoza <gleidymendoza@gmail.com>
2018-12-14 11:17:09 -0800

    Chapter 2.6: Arrow functions

--- app.ts
+++ app.ts
@@ -1,11 +1,37 @@
-// Chapter 2.5 For of loops
-var array = [
-  "Pick up drycleaning", 
-  "Clean Batcave", 
-  "Save Gotham"
-];
+// Chapter 2.6 Lambda Expressions or Arrow functions
 
-for (var index in array) {
-  var value = array[index];
-  console.log(`${index}: ${value}`);
+var container = document.getElementById('container');
+
+function Counter(el) {
+
+  this.count = 0;
+
+  el.innerHTML = this.count;
+
+  // el.addEventListener('click', 
+  //   function () {
+  //       this.count += 1; // 'this' refers to the global browser scope not to the one inside the function
+  //       el.innerHTML = this.count;
+  //   })
+
+  // An old solution is to create a reference to the function this and access it inside  
+  // let that = this;
+
+  // el.addEventListener('click', 
+  //   function () {
+  //       that.count += 1; 
+  //       el.innerHTML = that.count;
+  //   })
+
+  // With Arrow functions
+  
+  el.addEventListener('click', () => {
+    this.count += 1; // Now 'this' references to the Counter function
+    el.innerHTML = this.count;
+  })
 }
+
+new Counter(container);
+
+var filtered = [1, 2, 3].filter(x => x > 1);
+console.log(filtered);
