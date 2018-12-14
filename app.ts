d08dbccbb5745dbd8e0595e96ae1ea2309fa6bea
Gleidy Mendoza <gleidymendoza@gmail.com>
2018-12-14 12:27:22 -0800

    Chapter 2.9: Computed Properties

--- app.ts
+++ app.ts
@@ -1,57 +1,29 @@
-// Spread operator
+/*
+2.9 Computed properties:
+Allows you to define a property on an object with a name that is computed dynamically at runtime.
+*/
 
-// Here is not clear if this function accepts arguments
-function addEmpty() {
-  var values = Array.prototype.splice.call(arguments, [1]),
-      total = 0;
-console.log(values);
-  for (var value of values) {
-      total += value;
-  }
-  return total;
+/*
+Original Code
+var support = {
+  'os_Windows': isSupported('Windows'),
+  'os_iOS': isSupported('iOS'),
+  'os_Android': isSupported('Android'),
 }
 
-console.log("addEmpty", addEmpty());
+function isSupported(os) {
+  return Math.random() >= 0.5;
+}
+*/
 
-function add(...values) { // The spread operator must be the last argument
-  var total = 0;
+const osPrefix = 'os_';
 
-  for (var value of values) {
-      total += value;
-  }
-  return total;
+var support = {
+    [osPrefix + 'Windows']: isSupported('Windows'),
+    [osPrefix + 'iOS']: isSupported('iOS'),
+    [osPrefix + 'Android']: isSupported('Android'),
 }
 
-add(1, 2, 3);
-
-// Multiple parameters
-function calculate(action, ...values) { // The spread operator must be the last argument
-  var total = 0;
-
-  for (var value of values) {
-    switch (action) {
-      case 'add':
-        total += value;
-        break;
-      case 'substract':
-        total -= value;
-        break;
-    }  
-  }
-  return total;
+function isSupported(os) {
+    return Math.random() >= 0.5;
 }
-
-calculate('add', 2, 1, 4);
-
-// With arrays
-var source = [3, 4, 5];
-var target = [1, 2, ...source, 6, 7];
-console.log(target);
-
-// Concat arrays
-var list = [1, 2, 3], toAdd = [4, 5, 6];
-// Before
-// Array.prototype.push.apply(list, toAdd);
-// Now
-list.push(...toAdd);
-console.log("list", list);
